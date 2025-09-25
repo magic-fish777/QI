4747950d73c1062b93acb7a9bdbe58788c3c4be3
Ziyewin <wzyye129@163.com>
2025-09-25 11:44:26 +0800

    Create config.py

---
+++ wzy/roles/config.py
@@ -0,0 +1,17 @@
+# ==== API 配置 ====
+API_KEY = ""
+ASR_URL = ""
+TTS_URL = ""
+LLM_URL = ""
+
+# ==== 七牛云 OSS 配置 ====
+ACCESS_KEY = ""
+SECRET_KEY = ""
+BUCKET_NAME = ""
+
+DOMAIN = ""  # bucket绑定的测试域名
+
+HEADERS = {
+    "Authorization": f"Bearer {API_KEY}",
+    "Content-Type": "application/json"
+}
